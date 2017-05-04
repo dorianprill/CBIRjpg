167607707828304963425b46fb5faf82dbbdec0a
HugoPlatzer <hugoplatzer@aon.at>
2017-05-05 00:14:19 +0200

    structured output / code, fixed some bugs

--- retrieve.py
+++ retrieve.py
@@ -1,24 +1,11 @@
 import  cv2
 import  sys
 import  os
+import  glob
 import  pickle
 import  numpy as np
 
-
-if __name__ == "__main__":
-    #Walk over data directory containing the source images in a
-    #subdirectory according to their class
-    #./data
-        #'---> class1/
-            #'---> img1.png
-            #'---> img2.png
-        #'---> class2/
-            #'---> img1.png ...
-    # root directory of the datasets
-    queryDir = sys.argv[1]
-    dataDir = sys.argv[2]
-    descriptor = sys.argv[3]
-
+def createMatcher(descriptor):
     if descriptor == 'sift' or descriptor == 'surf' or descriptor == 'kaze':
         bf = cv2.BFMatcher()
         FLANN_INDEX_KDTREE = 1
@@ -33,39 +20,63 @@ if __name__ == "__main__":
                    multi_probe_level = 1) #2
 
     search_params = dict(checks=50)
-    imagesToRetrieve = 5
     flann = cv2.FlannBasedMatcher(index_params,search_params)
+    
+    return bf
+
+def descriptorNameCategory(descriptorFilename):
+    dSplit = descriptorFilename.split("/")
+    return (dSplit[-1], dSplit[-2])
+
+def printResults(queryDescriptorFilename, results):
+    queryName, queryCategory = descriptorNameCategory(queryDescriptorFilename)
+    print("query={} category={}".format(queryName, queryCategory))
+    results = sorted(results)
+    for result in results:
+        trainName, trainCategory = descriptorNameCategory(result[1])
+        print("train={} category={} distance={}".format(trainName, trainCategory, result[0]))
+
+def getDescriptors(directory, descriptorType):
+    descriptorFilenames = glob.glob(os.path.join(directory, "*/*." + descriptorType))
+    return sorted((name, loadDescriptor(name)) for name in descriptorFilenames)
+
+def loadDescriptor(descriptorFile):
+    return pickle.load(open(descriptorFile, 'rb'))
+
+def doMatching(queryDescriptor, trainDescriptor, matcher):
+    minRatio = 0.9
+    matches = bf.knnMatch(queryDescriptor, trainDescriptor, k = 2)
+    goodMatches = [m for m, n in matches if m.distance < minRatio * n.distance]
+    if len(goodMatches) == 0:
+        raise ValueError("no matches survived ratio test")
+    return np.average([m.distance for m in goodMatches])
 
 
-    for subdir, dirs, files in os.walk(queryDir):
-        for file in files:
-            queryFilename = os.path.join(subdir, file)
-            if file.endswith('.' + descriptor):
-                print('queryDes:' + queryFilename);
-                queryDes = pickle.load(open(queryFilename, 'rb'))
-                queryType = queryFilename.split('/')[-2]
-                goodMatches = []
-                results = []
-                
-                for subdir2, dirs, files2 in os.walk(dataDir):
-                    for file2 in files2:
-                        if file2.endswith('.' + descriptor):
-                            # find the keypoints and compute descriptors
-                            print('des: ' + os.path.join(subdir2, file2))
-                            des = pickle.load(open(os.path.join(subdir2, file2), 'rb'))
-                            matches = bf.knnMatch(queryDes, des, k=2)
-                            for m,n in matches:
-                                if m.distance < 0.70*n.distance:
-                                    goodMatches.append(m)
+if __name__ == "__main__":
+    #Walk over data directory containing the source images in a
+    #subdirectory according to their class
+    #./data
+        #'---> class1/
+            #'---> img1.png
+            #'---> img2.png
+        #'---> class2/
+            #'---> img1.png ...
+    # root directory of the datasets
+    queryDir = sys.argv[1]
+    trainDir = sys.argv[2]
+    descriptorType = sys.argv[3]
 
-                            distances = list(map(lambda m: m.distance, goodMatches))
-                            result =  (np.average(distances), subdir2.split('/')[-1])
-                            print('result: ' + str(result))
-                            results.append(result)
+    bf = createMatcher(descriptorType)
+    queryDescriptors = getDescriptors(queryDir, descriptorType)
+    trainDescriptors = getDescriptors(trainDir, descriptorType)
 
-                print("queryType: " + queryType)
-                correctImages = len(list(filter(lambda r: r[1] == queryType , sorted(results)[:imagesToRetrieve])))
+    for queryDesFile, queryDes in queryDescriptors:       
+        results = []
+        
+        for trainDesFile, trainDes in trainDescriptors:
+        
+            trainDesDist = doMatching(queryDes, trainDes, bf)
+            results.append((trainDesDist, trainDesFile))
 
-                print("total: " + str(imagesToRetrieve) + " found: " + str(correctImages))
-
-                print(sorted(results))
+        printResults(queryDesFile, results)
+        print("")
