b92d00a7b076d8b8cfcc51055876139f29cf4b1a
Dorian Prill <dorian.prill@gmail.com>
2017-05-05 13:10:13 +0200

    removed mser and fast from retrieval script as they are NOT descriptors but only keypoints

--- retrieveEverything.sh
+++ retrieveEverything.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dataDirectory=data/test
-descriptors=(sift surf mser fast brief brisk kaze orb)
+descriptors=(sift surf brief brisk kaze orb) # TODO hog
 
 for descriptor in "${descriptors[@]}"; do
     result=$(python3 retrieve.py $dataDirectory $dataDirectory $descriptor | python3 avgArea.py)
