aeb5f63f6ed268d1e26f5d1b27eb7e43bf273a33
Michael Otteneder <m.otteneder@gmail.com>
2017-05-29 17:07:29 +0200

    removed obsolete scripts

--- retrieveEverything.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dataDirectory=data/test
-descriptors=(sift surf brief orb hog brisk kaze) # TODO hog
-
-for descriptor in "${descriptors[@]}"; do
-    result=$(python3 retrieve.py $dataDirectory $dataDirectory $descriptor | python3 avgArea.py)
-    echo $descriptor $result
-done
