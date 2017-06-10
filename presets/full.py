55298f307a02153a5549b6e2f22f67e82a17774a
Michael Otteneder <m.otteneder@gmail.com>
2017-06-10 22:53:17 +0200

    reduced max compression rate to avoid errors

--- presets/full.py
+++ presets/full.py
@@ -1,8 +1,8 @@
 plots = [
-    {"compression" : "jpg", "scenario" : "tcqc", "ratios" : [1, 2, 10, 20, 50, 100, 150, 200, 250, 350, 450, 500], "descriptors" : ["orb", "sift", "surf", "brief", "brisk", "kaze"]},
-    {"compression" : "jpg", "scenario" : "tcqu", "ratios" : [1, 2, 10, 20, 50, 100, 150, 200, 250, 350, 450, 500], "descriptors" : ["orb", "sift", "surf", "brief", "brisk", "kaze"]},
-    {"compression" : "jxr", "scenario" : "tcqc", "ratios" : [1, 2, 10, 20, 50, 100, 150, 200, 250, 350, 450, 500], "descriptors" : ["orb", "sift", "surf", "brief", "brisk", "kaze"]},
-    {"compression" : "jxr", "scenario" : "tcqu", "ratios" : [1, 2, 10, 20, 50, 100, 150, 200, 250, 350, 450, 500], "descriptors" : ["orb", "sift", "surf", "brief", "brisk", "kaze"]},
-    {"compression" : "jp2", "scenario" : "tcqc", "ratios" : [1, 2, 10, 20, 50, 100, 150, 200, 250, 350, 450, 500], "descriptors" : ["orb", "sift", "surf", "brief", "brisk", "kaze"]},
-    {"compression" : "jp2", "scenario" : "tcqu", "ratios" : [1, 2, 10, 20, 50, 100, 150, 200, 250, 350, 450, 500], "descriptors" : ["orb", "sift", "surf", "brief", "brisk", "kaze"]}
+    {"compression" : "jpg", "scenario" : "tcqc", "ratios" : [1, 2, 10, 20, 50, 100, 150, 200, 250], "descriptors" : ["orb", "sift", "surf", "brief", "brisk", "kaze"]},
+    {"compression" : "jpg", "scenario" : "tcqu", "ratios" : [1, 2, 10, 20, 50, 100, 150, 200, 250], "descriptors" : ["orb", "sift", "surf", "brief", "brisk", "kaze"]},
+    {"compression" : "jxr", "scenario" : "tcqc", "ratios" : [1, 2, 10, 20, 50, 100, 150, 200, 250], "descriptors" : ["orb", "sift", "surf", "brief", "brisk", "kaze"]},
+    {"compression" : "jxr", "scenario" : "tcqu", "ratios" : [1, 2, 10, 20, 50, 100, 150, 200, 250], "descriptors" : ["orb", "sift", "surf", "brief", "brisk", "kaze"]},
+    {"compression" : "jp2", "scenario" : "tcqc", "ratios" : [1, 2, 10, 20, 50, 100, 150, 200, 250], "descriptors" : ["orb", "sift", "surf", "brief", "brisk", "kaze"]},
+    {"compression" : "jp2", "scenario" : "tcqu", "ratios" : [1, 2, 10, 20, 50, 100, 150, 200, 250], "descriptors" : ["orb", "sift", "surf", "brief", "brisk", "kaze"]}
 ]
